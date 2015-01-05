41e0b7b7455cbf2e51da3397282c1a37b1752cc1
Karl Palsson <karlp@tweak.net.au>
2015-01-05 20:50:15 +0000

    options: Properly accept -h for help
    Help text referred to an option that wasn't usable.
    
    Signed-off-by: Karl Palsson <karlp@tweak.net.au>

--- linux-serial-test.c
+++ linux-serial-test.c
@@ -160,7 +160,7 @@ void process_options(int argc, char * argv[])
 {
 	for (;;) {
 		int option_index = 0;
-		static const char *short_options = "b:p:d:R:TsSy:z:certlq:";
+		static const char *short_options = "hb:p:d:R:TsSy:z:certlq:";
 		static const struct option long_options[] = {
 			{"help", no_argument, 0, 0},
 			{"baud", required_argument, 0, 'b'},
